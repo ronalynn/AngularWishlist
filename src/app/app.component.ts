0bde2bc43266470be283c5f39f6512a232041615
ronalynn <ronalynnanong@gmail.com>
2024-01-02 11:32:40 +0000

    created a wishlist constructor and added wishlist items to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [
+    new WishItem('Complete Angular tutorial'),
+    new WishItem('Book hair appointment', true),
+    new WishItem('Read two chapters of Red Rising')
+  ];
   title = 'Ronalyn!';
 }
